ec5dd67d4906977353cfd323f09639a92ee9c4d6
seaona <mariona@gmx.es>
2023-11-24 09:59:19 +0100

    functions, primitive, variables, if solutions

--- rust/homeworks/homework3/variables/variables1.rs
+++ rust/homeworks/homework3/variables/variables1.rs
@@ -2,9 +2,8 @@
 // Make me compile!
 // Execute the command `zustlings hint variables1` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
+    let mut y : u32;
     y = 5;
     println!("y has the value {}", y);
 }
